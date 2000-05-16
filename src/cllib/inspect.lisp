0cc2e54bf35684ddab57e2b4046c7568beb0ad71
sds <unknown>
2000-05-16 17:22:47 +0000

    (inspection): fixed type for `nth-slot'

--- src/cllib/inspect.lisp
+++ src/cllib/inspect.lisp
@@ -55,7 +55,7 @@
   (up nil :type (or null inspection)) ; parent
   (num-slots 0 :type fixnum)    ; the number of slots
   (pos nil :type (or null fixnum)) ; pos in parent
-  (nth-slot nil :type (or null (function (integer) (t t)))) ; value & name
+  (nth-slot nil :type (or null (function (integer) (values t t)))) ; val & name
   (set-slot nil :type (or null (function (integer t) t)))) ; set Nth slot
 )
 
