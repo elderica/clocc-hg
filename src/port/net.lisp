04eb64c4be4e0883b564d8f402e59c6429737d9f
sds <unknown>
2001-07-16 16:13:38 +0000

    (open-socket, socket-accept): CMUCL: specify :buffering :line  Reported by Janne Rinta-Manty <rintaman@cs.Helsinki.FI>

--- src/port/net.lisp
+++ src/port/net.lisp
@@ -122,6 +122,7 @@
                        port host :element-type
                        (if bin '(unsigned-byte 8) 'character))
     #+cmu (sys:make-fd-stream (ext:connect-to-inet-socket host port)
+                              :buffering :line
                               :input t :output t :element-type
                               (if bin '(unsigned-byte 8) 'character))
     #+gcl (si:make-socket-stream host port bin) ; FIXME
@@ -242,6 +243,7 @@ Returns a socket stream or NIL."
                          (if bin '(unsigned-byte 8) 'character))))
   #+cmu (when (sys:wait-until-fd-usable serv :input wait)
           (sys:make-fd-stream (ext:accept-tcp-connection serv)
+                              :buffering :line
                               :input t :output t :element-type
                               (if bin '(unsigned-byte 8) 'character)))
   #+gcl (si:accept-socket-connection serv bin wait) ; FIXME
