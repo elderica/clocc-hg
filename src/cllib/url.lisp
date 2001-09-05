c9c8ae5ae904e6e3d79bedb7b40c65850df7af95
sds <unknown>
2001-09-05 21:34:02 +0000

    (url-path-parse): new function (url-path-dir, url-path-file, url-path-args): use it

--- src/cllib/url.lisp
+++ src/cllib/url.lisp
@@ -36,7 +36,8 @@
 
 (export '(url url-ask url-eta protocol-rfc
           url-prot url-user url-pass url-host url-port url-path
-          url-get-host url-get-port url-path-dir url-path-file
+          url-get-host url-get-port url-path-parse
+          url-path-dir url-path-file url-path-args
           open-socket-retry open-url with-open-url
           ftp-list url-send-mail url-get-news url-time
           browse-url *browsers* *browser*
@@ -117,17 +118,28 @@ guess from the protocol."
   (if (plusp (length (url-host url))) (url-host url)
       (case (url-prot url) ((:news :nntp) *nntpserver*))))
 
-(defun url-path-dir (url)
-  "Return the dir part of the url's path, dropping the file name."
+(defun url-path-parse (url)
+  "Parse the path of URL, returning 3 values: DIR, FILE and ARGS."
   (setq url (url url))
-  (subseq (url-path url) 0
-          (1+ (or (position #\/ (url-path url) :from-end t) -1))))
+  (let* ((path (url-path url))
+         (?pos (position #\? path))
+         (/pos (1+ (or (position #\/ path :from-end t :end ?pos) -1))))
+    (values
+     (subseq path 0 /pos)       ; path
+     (subseq path /pos ?pos)    ; file
+     (if ?pos (subseq path ?pos) "")))) ; args
 
-(defun url-path-file (url)
-  "Return the file part of the url's path, dropping the dir."
-  (setq url (url url))
-  (subseq (url-path url)
-          (1+ (or (position #\/ (url-path url) :from-end t) -1))))
+(defsubst url-path-dir (url)
+  "Return the dir part of the URL's path."
+  (value (url-path-parse url)))
+
+(defsubst url-path-file (url)
+  "Return the file part of the URL's path."
+  (nth-value 1 (url-path-parse url)))
+
+(defsubst url-path-args (url)
+  "Return the args part of the URL's path."
+  (nth-value 2 (url-path-parse url)))
 
 (defmethod print-object ((url url) (out stream))
   "Print the URL in the standard form."
