d65f83db144f75cc8466e826941cc20a88d2c25f
cvs2hg <unknown>
2002-03-29 20:44:55 +0000

    fixup commit for tag 'release-2002-03-29'

--- src/defsystem-4/src/impl-dependent/kcl-derivatives.lisp
+++
@@ -1,8 +0,0 @@
-;;; -*- Mode: CLtL -*-
-
-(in-package "MK4")
-
-(defun run-program (program &rest arguments)
-  (system (format nil "~A~@[~{ ~A~}~]" program arguments)))
-
-;;; end of file -- kcl-derivatives.lisp --
