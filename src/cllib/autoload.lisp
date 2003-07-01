cf1a13ce4e083cd5642df0d6679e94ae1bac2ab8
sds <unknown>
2003-07-01 19:26:39 +0000

    (autoload) [CLISP]: set SYS::FILE documentation to avoid the function redefinition warning

--- src/cllib/autoload.lisp
+++ src/cllib/autoload.lisp
@@ -27,16 +27,18 @@
   (declare (symbol symb) (type (or simple-string null) comment))
   (export (list symb))
   (unless (fboundp symb)
-    (setf (fdefinition symb)
-          (lambda (&rest args)
-            (setf (documentation symb 'function) nil)
-            (fmakunbound symb)
-            (format t "; ~s is being autoloaded from `~a'~%" symb file)
-            (require file (translate-logical-pathname
-                           (concatenate 'string "cllib:" file)))
-            (apply (fdefinition symb) args))
-          (documentation symb 'function)
-          (format nil "Autoloaded (from ~a)~@[:~%~a~]" file comment))))
+    (let ((path (translate-logical-pathname
+                 (concatenate 'string "cllib:" file))))
+      (setf (fdefinition symb)
+            (lambda (&rest args)
+              (setf (documentation symb 'function) nil)
+              (fmakunbound symb)
+              (format t "; ~s is being autoloaded from `~a'~%" symb file)
+              (require file path)
+              (apply (fdefinition symb) args))
+            #+clisp (documentation symb 'sys::file) #+clisp path
+            (documentation symb 'function)
+            (format nil "Autoloaded (from ~a)~@[:~%~a~]" file comment)))))
 
 )
 
