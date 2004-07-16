bb31dd2ad4add81223abb68b30b46f5e186dc651
sds <unknown>
2004-07-16 17:23:19 +0000

    (test-xml): cl-ent now has 1183 entities

--- src/cllib/tests.lisp
+++ src/cllib/tests.lisp
@@ -194,7 +194,7 @@
                  (incf num-err)))))
       (mesg :test out " ** ~s...~%" 'test-xml)
       (ts *xml-ent-file* 283)
-      (ts (translate-logical-pathname "clocc:etc;cl-ent.xml") 1177))
+      (ts (translate-logical-pathname "clocc:etc;cl-ent.xml") 1183))
     (mesg :test out " ** ~s: ~:d error~:p~2%" 'test-xml num-err)
     num-err))
 
