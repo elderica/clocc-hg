5fa42945ac51485a21d63664735ef800e857fadf
sds <unknown>
2007-05-06 02:09:12 +0000

    (plot-columns): accept :KEY1 and :KEY2

--- src/cllib/data.lisp
+++ src/cllib/data.lisp
@@ -400,12 +400,14 @@ Everything is allocated anew."
     ht))
 
 (defun plot-columns (table col1 col2 &rest options
-                     &key by (out *standard-output*) &allow-other-keys
-                     &aux (*gnuplot-msg-stream* out))
+                     &key by (out *standard-output*)
+                          (key1 #'identity) (key2 #'identity)
+                     &allow-other-keys &aux (*gnuplot-msg-stream* out))
   "Plot one column vs the other."
   (multiple-value-bind (c1 n1 s1) (column-name-sc col1 table)
     (multiple-value-bind (c2 n2 s2) (column-name-sc col2 table)
-      (flet ((f (l) (mapcar (lambda (v) (cons (aref v c1) (aref v c2))) l)))
+      (flet ((f (l) (mapcar (lambda (v) (cons (funcall key1 (aref v c1))
+                                              (funcall key2 (aref v c2)))) l)))
         (apply #'plot-lists-arg
                (if by
                    (let (l)
