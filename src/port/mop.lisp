77a1dcc4095da393a547a28ee01aee02bafc922b
sds <unknown>
2007-06-25 20:57:21 +0000

    fix allegro read-time conditions

--- src/port/mop.lisp
+++ src/port/mop.lisp
@@ -1,6 +1,6 @@
 ;;;; Meta-Object Protocol
 ;;;
-;;; Copyright (C) 2006 by Sam Steingold
+;;; Copyright (C) 2006-2007 by Sam Steingold
 ;;; This is open-source software.
 ;;; GNU Lesser General Public License (LGPL) is applicable:
 ;;; No warranty; you may copy/modify/redistribute under the same
@@ -19,7 +19,7 @@
   (require :port-ext (translate-logical-pathname "clocc:src;port;ext")))
 
 (in-package
-  #+(and allegro (not (version>= 6))) :clos
+  #+(and allegro (version>= 6)) :clos
   #+(and clisp mop) :clos
   #+cmu :pcl
   #+lucid :clos
