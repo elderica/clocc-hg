4264d782942720020b711285487a35041dbe7e04
rtoy <unknown>
2008-02-25 19:27:38 +0000

    Fix typo in description of integral for tst1.

--- src/f2cl/packages/quadpack/quadpack-tests.lisp
+++ src/f2cl/packages/quadpack/quadpack-tests.lisp
@@ -3,6 +3,10 @@
 ;;;
 ;;; $Id$
 ;;; $Log$
+;;; Revision 1.5  2006/04/28 01:35:12  rtoy
+;;; In TST17 for DQAWC, the absolute error criterion was too small.  It
+;;; must be strictly positive.  This test runs as expected.
+;;;
 ;;; Revision 1.4  2002/03/19 23:12:33  rtoy
 ;;; It's f2cl-lib:integer4, not just plain integer4.
 ;;;
@@ -33,8 +37,8 @@
   (abs (- est true)))
 
 ;; Test 1
-;; Compute integral e^(alpha*x)*log(1/x) from 0 to 1.  The analytical
-;; solution is e^((1+alpha)^(-2)).
+;; Compute integral (x^alpha)*log(1/x) from 0 to 1.  The analytical
+;; solution is (1+alpha)^(-2).
 ;;
 ;; alpha = -0.9(0.1)0(0.2)2.6
 ;; QAG with key 1, 3, 6
@@ -64,9 +68,9 @@
     (format t "Test integral 1:~%")
     (format t "
            1
-          /                                      -2
-          [    alpha x                (alpha + 1) 
-        - I  %E        LOG(x) dx =  %E            
+          /                                           -2
+          [    alpha log(x)                (alpha + 1) 
+        - i  %e            log(x) dx =  %e            
           ]
           /
            0
