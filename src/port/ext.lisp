b6700bce581ced6ecbd12fcb116349fe3a5ac92a
Sam Steingold <sds@podval.org>
2014-02-24 15:25:21 -0500

    (quit) [sbcl]: use sb-ext:exit instead of sb-ext:quit

--- src/port/ext.lisp
+++ src/port/ext.lisp
@@ -1,6 +1,6 @@
 ;;; Basic extensions: conditions, compositions &c
 ;;;
-;;; Copyright (C) 1999-2008, 2010 by Sam Steingold
+;;; Copyright (C) 1999-2008, 2010, 2013 by Sam Steingold
 ;;; This is open-source software.
 ;;; GNU Lesser General Public License (LGPL) is applicable:
 ;;; No warranty; you may copy/modify/redistribute under the same
@@ -121,7 +121,7 @@ Inspired by Paul Graham, <On Lisp>, p. 145."
   #+gcl (lisp:bye code)
   #+lispworks (lw:quit :status code)
   #+lucid (lcl:quit code)
-  #+sbcl (sb-ext:quit :unix-status
+  #+sbcl (sb-ext:exit :code
                       (typecase code ((signed-byte 32) code) (null 0) (t 1)))
   #-(or allegro clisp cmu cormanlisp gcl lispworks lucid sbcl)
   (error 'not-implemented :proc (list 'quit code)))
